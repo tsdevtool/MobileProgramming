c9dc98ec832163b046854255f94f54fc55096adb
ThanhSieu2k3 <122494878+tsdevtool@users.noreply.github.com>
2024-08-09 12:18:31 +0700

    End Date 8

--- SQLite_Ex2/app/src/main/java/com/crissiiu/sqlite_ex2/db.java
+++ SQLite_Ex2/app/src/main/java/com/crissiiu/sqlite_ex2/db.java
@@ -1,6 +1,7 @@
 package com.crissiiu.sqlite_ex2;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,17 +10,48 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class db extends SQLiteOpenHelper {
-    public db(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public static final String DB_NAME = "product.sqlite";
+    public static final int VERSION = 1;
+    public static final String TBL_NAME = "Product";
+    public static final String COL_PRODUCT_ID = "ProductId";
+    public static final String COL_PRODUCT_NAME  = "ProductName";
+    public static final String COL_PRODUCT_PRICE = "ProductPrice";
+
+
+    public db(@Nullable Context context) {
+        super(context, DB_NAME, null, VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        String sql = "CREATE TABLE IF NOT EXISTS " + TBL_NAME + " ( "
+                + COL_PRODUCT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_PRODUCT_NAME + " TEXT(50), "
+                + COL_PRODUCT_PRICE + " REAL)";
+        sqLiteDatabase.execSQL(sql);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        String sql = "DROP TABLE IF EXISTS " + TBL_NAME;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
     }
+
+
+//    Select
+    public Cursor queryData(String sql){
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery(sql, null);
+    }
+
+//    Insert
+    public void execSql(String sql){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL(sql);
+    }
+//    Update
+
+//    Delete
 }
